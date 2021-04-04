92690ad9478b94630256c9d197c955b99b3f1f84
Jonathan Schwartz <>
2021-04-04 15:45:55 -0400

    Board visualizer working

---
+++ chessBot.py
@@ -0,0 +1,18 @@
+import numpy as np
+import chess
+
+class chessBot:
+    board = None
+    def __init__(self):
+        self.board = chess.Board()
+
+    def showBoard(self):
+        print(self.board)
+
+
+
+# currently just for testing
+if __name__ == "__main__":
+    print('oi')
+    test = chessGame()
+    test.showBoard()
